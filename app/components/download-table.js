a96350cfb0a5c069334eab4c90819c40df2e1bc8
Benjamin Herson <benjaminherson@gmail.com>
2021-03-07 19:28:38 -0800

    add data to chart
    Data set in a utility file.
    Add status indicator dots.

--- app/components/download-table.js
+++ app/components/download-table.js
@@ -1,3 +1,6 @@
 import Component from '@glimmer/component';
+import { DOWNLOAD_DATA } from '../utils/download-data'
 
-export default class DownloadTableComponent extends Component {}
+export default class DownloadTableComponent extends Component {
+  data = DOWNLOAD_DATA;
+}
